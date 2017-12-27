2b2a662d421c86bf470d84dce5b122ae461c6c57
ozkangokdere <ozkangokdere@gmail.com>
2017-12-27 21:54:02 +0300

    Add input control to short time Fourier transform function.

--- stft.m
+++ stft.m
@@ -1,1 +1,28 @@
-function output = stft(signal, windowLength, windowType, windowShiftLength)
+function output = stft(signal, window, windowShiftLength)
+
+signalLength = size(signal);
+if(length(signalLength) ~= 2 || min(signalLength) ~=1)
+    error('Input signal must be a vector.');
+end
+signalLength = max(signalLength);
+
+windowLength = size(window);
+if(length(windowLength) ~= 2 || min(windowLength) ~=1)
+    error('Window must be a vector.');
+end
+windowLength = max(windowLength);
+
+if(windowLength >= signalLength)
+   error('Signal length must be much larger than window length.') 
+end
+
+if(~isscalar(windowShiftLength) || floor(windowShiftLength) ~= windowShiftLength || windowShiftLength <= 0)
+    error('Shift value between consecutive windows should be a positive scalar integer.')
+end
+
+if(windowShiftLength > windowLength)
+    error('Shift between consecutive windows should at most be equal to window length.');
+end
+
+signal = reshape(signal, 1, signalLength);
+window = reshape(window, 1, windowLength);
