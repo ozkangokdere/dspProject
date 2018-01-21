6fdc4878adba60b3ffc1530b8772f5a27b2bb1ed
ozkangokdere <ozkangokdere@gmail.com>
2018-01-21 18:28:17 +0300

    Add rectangle windowed linear chirp generation function.  Add usage of this in runScript.m.

--- runScript.m
+++ runScript.m
@@ -1,15 +1,16 @@
 fs = 48e3;          % Sampling frequency [Hz]
 duration = 0.5;     % Signal length in [seconds]
-frequency = 1e3;   % Signal frequency [Hz]
-phase = pi/6;       % Signal phase at t = 0 [radian]
+frequency = 10e3;   % Signal frequency [Hz]
+phase = 0;       % Signal phase at t = 0 [radian]
 amplitude = 0.5;    %  Signal amplitude [V]
 
-signal = generateSinusoidalSignal(frequency, phase, amplitude, fs, duration);
+singleToneSignal = generateSinusoidalSignal(frequency, phase, amplitude, fs, duration);
+linearChirpSignal = generateLinearChirpSignal(frequency, frequency*100, phase, amplitude, fs, duration);
 
 windowName = 'hamming';
 windowLength = 256;
 shiftLength = 16;
-spectrogram = getSpectrogram(signal, windowName, windowLength, shiftLength);
-
-plotSignalnSpectrogram(signal, spectrogram, fs, duration, windowLength, shiftLength);
-
+singleToneSpectrogram = getSpectrogram(singleToneSignal, windowName, windowLength, shiftLength);
+linearChirpSpectrogram = getSpectrogram(linearChirpSignal, windowName, windowLength, shiftLength);
+plotSignalnSpectrogram(singleToneSignal, singleToneSpectrogram, fs, duration, windowLength, shiftLength); title('Single Tone Signal & Spectrogram')
+plotSignalnSpectrogram(linearChirpSignal, linearChirpSpectrogram, fs, duration, windowLength, shiftLength); title('Linear Chirp Signal & Spectrogram')
