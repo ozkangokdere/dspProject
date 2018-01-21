76bfa48d2b3be3b4c486277e62dcd6b1b4c045cc
ozkangokdere <ozkangokdere@gmail.com>
2018-01-22 00:23:31 +0300

    Add read audio file and demonstrate in runScript. This cannot work in octave most probably

--- runScript.m
+++ runScript.m
@@ -4,6 +4,8 @@ frequency = 10e3;   % Signal frequency [Hz]
 phase = 0;       % Signal phase at t = 0 [radian]
 amplitude = 0.5;    %  Signal amplitude [V]
 
+fileName = 'hopi.mp3';
+leftOrRight = 1; % 1 is for left 2 is for right
 singleToneSignal = generateSinusoidalSignal(frequency, phase, amplitude, fs, duration);
 linearChirpSignal = generateLinearChirpSignal(frequency, frequency*100, phase, amplitude, fs, duration);
 
@@ -12,5 +14,7 @@ windowLength = 256;
 shiftLength = 16;
 singleToneSpectrogram = getSpectrogram(singleToneSignal, windowName, windowLength, shiftLength);
 linearChirpSpectrogram = getSpectrogram(linearChirpSignal, windowName, windowLength, shiftLength);
+audioFileSpectrogram = getSpectrogram(y(:,leftOrRight), windowName, windowLength, shiftLength);
 plotSignalnSpectrogram(singleToneSignal, singleToneSpectrogram, fs, duration, windowLength, shiftLength); title('Single Tone Signal & Spectrogram')
 plotSignalnSpectrogram(linearChirpSignal, linearChirpSpectrogram, fs, duration, windowLength, shiftLength); title('Linear Chirp Signal & Spectrogram')
+plotSignalnSpectrogram(y(:,leftOrRight), audioFileSpectrogram, samplingRateFile, length(y(:,1)) / samplingRateFile, windowLength, shiftLength); title('Audio File Signal & Spectrogram')
