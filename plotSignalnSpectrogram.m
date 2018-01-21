06cab226cb1d30ab729165dfce96895587fec59d
ozkangokdere <ozkangokdere@gmail.com>
2018-01-21 17:31:37 +0300

    Fix the mistake of frequency domain limit plot.

--- plotSignalnSpectrogram.m
+++ plotSignalnSpectrogram.m
@@ -5,7 +5,7 @@ timeVector = (0:duration*fs) / fs;
 [ax1Len , ax2Len] = size(spectrogram);
 timeVector2 = 0:shiftLength/fs:(signalLength-windowLength-1)/fs;
 ax1 = repmat(timeVector2, ax1Len, 1);
-ax2 = repmat(((0:ax1Len-1)/ax1Len*fs)', 1, ax2Len);
+ax2 = repmat(((0:ax1Len-1)/ax1Len*fs/2)', 1, ax2Len);
 figure,
 subplot(2,1,1)
 plot(timeVector, signal); xlabel('Time [seconds]'), ylabel('Magnitude [Volts]')
